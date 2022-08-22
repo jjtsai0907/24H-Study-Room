c4254ebaf209672c8a25b6163fb7ea1cdb577356
JJ Tsai <jj.tsai@b3.se>
2022-08-22 15:53:04 +0200

    Initial Commit

---
+++ 24H Study Room/_4H_Study_RoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4H_Study_RoomApp.swift
+//  24H Study Room
+//
+//  Created by Jia-Jiuan Tsai on 2022-08-22.
+//
+
+import SwiftUI
+
+@main
+struct _4H_Study_RoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
